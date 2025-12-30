9693c7b2acc6f463ff3065a9a702d9bc0786a167
muthuselvi12 <smuthuselvi965@gmail.com>
2025-12-30 17:18:14 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,21 @@
-
-
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow">
       <div className="container">
-          <a className="navbar-brand fw-bold fs-4" href="#home">
-          <img 
-            src='/dmcreativestudio.png' 
-            alt="DM Creative Studio" 
-            style={{ height: "45px", width: "auto" }}
-          />
+
+        <a className="navbar-brand fw-bold fs-4" href="#home">
+          DM CREATIVE STUDIO
         </a>
 
-        <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#menu"
+          aria-controls="menu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
@@ -22,10 +25,15 @@ export default function Navbar() {
             <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
             <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
             <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-            <li className="nav-item"><a className="nav-link btn btn-warning px-3 text-dark" href="#contact">Contact</a></li>
+            <li className="nav-item">
+              <a className="btn btn-warning px-3 text-dark nav-link" href="#contact">
+                Contact
+              </a>
+            </li>
           </ul>
         </div>
+
       </div>
     </nav>
-  )
+  );
 }
